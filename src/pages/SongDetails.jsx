c6851a9f4eb0a3d71048803c0e4194a396b6769e
Taku10 <tvmadziwa5@gmail.com>
2022-12-05 17:32:16 +0200

    init

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -14,10 +14,10 @@ const SongDetails = () => {
         <div>
             <DetailsHeader artistId=''songData={songData} />
             <div className='mb-10'>
-                <h2 className='text-white text-3xl font0bold'>Lyrics:</h2>
+                <h2 className='text-white text-3xl font-bold'>Lyrics:</h2>
                 <div className='mt-5'>
                     {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (
-                        <p className='text-gray-400 text-base my-1'>{line}</p>)) : <p className='text-gray-400 text-base my-1'>Sorry, no lyrics found</p>}
+                        <p className='text-gray-400  text-base my-1'>{line}</p>)) : <p className='text-gray-400  text-base my-1'>Sorry, no lyrics found</p>}
                 </div>
             </div>
         </div>
